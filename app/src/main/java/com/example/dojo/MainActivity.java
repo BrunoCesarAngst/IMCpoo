7504bfd437bbae7cdafb53ceaa1272b16363ff72
DouglasSerena <douglas222serena@rede.ulbra.br>
2021-05-19 22:26:17 -0300

    format resultado

--- app/src/main/java/com/example/dojo/MainActivity.java
+++ app/src/main/java/com/example/dojo/MainActivity.java
@@ -10,9 +10,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    EditText pesoEditText;
+    EditText alturaEditText;
+    TextView resultadoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,48 +24,59 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Objects.requireNonNull(getSupportActionBar()).hide();
 
-        TextView resultadoTextView = (TextView) findViewById(R.id.textResultado);
-        float peso = (Float) Float.parseFloat(getIntent().getStringExtra("PESO"));
-        float altura = (Float) Float.parseFloat(getIntent().getStringExtra("ALTURA"));
-        float textResultado = peso/(altura*altura);
-        if(textResultado<18.5){
-            resultadoTextView.setText("Seu IMC é " + textResultado + ". Está abaixo do ideal");
-        }else if(textResultado>18.6 && textResultado<24.9){
-            resultadoTextView.setText("Seu IMC é " + textResultado + ". Está no peso ideal");
-        }else if(textResultado>25.0 && textResultado<29.9){
-        resultadoTextView.setText("Seu IMC é " + textResultado + ". Está levemente acima do peso ideal");
-        }else if(textResultado>30.0 && textResultado<34.9){
-            resultadoTextView.setText("Seu IMC é " + textResultado + ". Está com obesidade grau 1");
-        }else if(textResultado>35.0 && textResultado<39.9){
-            resultadoTextView.setText("Seu IMC é " + textResultado + ". Está com obesidade grau 2 (severa)");
-        } else{
-            resultadoTextView.setText("Seu IMC é " + textResultado + ". Está com obesidade grau 3 (mórbida)");
-        }
+        pesoEditText = (EditText) findViewById(R.id.peso);
+        alturaEditText = (EditText) findViewById(R.id.altura);
+        resultadoTextView = (TextView) findViewById(R.id.textResultado);
 
         Button btnCalcular = (Button) findViewById(R.id.btnCalcular);
         btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText pesoEditText = (EditText) findViewById(R.id.peso);
-                EditText alturaEditText = (EditText) findViewById(R.id.altura);
 
-                if(pesoEditText.getText().length()==0 || alturaEditText.getText().length()==0){
+                if(
+                    pesoEditText.getText().length()==0 ||
+                    alturaEditText.getText().length()==0
+                ){
                     Toast.makeText(
-                            getApplicationContext(),"Por favor preencha o campo",
+                            getApplicationContext(),
+                            "Por favor preencha o campo",
                             Toast.LENGTH_SHORT
                     ).show();
                 }else{
-                    float peso = (Float) Float.parseFloat(pesoEditText.getText().toString());
-                    float altura = (Float) Float.parseFloat(alturaEditText.getText().toString());
+                    double peso = (Double) Double.parseDouble(
+                            pesoEditText.getText().toString());
+                    double altura = (Double) Double.parseDouble(
+                            alturaEditText.getText().toString());
 
                     if(peso==0 || altura==0.0){
                         Toast.makeText(
                                 getApplicationContext(),"Preencha um valor válido",
                                 Toast.LENGTH_SHORT
                         ).show();
+                    } else {
+                        calcular(peso, altura);
                     }
                 }
             }
         });
+    }
+
+    public void calcular(double peso, double altura) {
+        DecimalFormat df = new DecimalFormat("#.00");
+        double textResultado = peso / (altura * altura);
+        if(textResultado<18.5){
+            resultadoTextView.setText("Seu IMC é " + df.format(textResultado) + ". Está abaixo do ideal");
+        }else if(textResultado>18.6 && textResultado<24.9){
+            resultadoTextView.setText("Seu IMC é " + df.format(textResultado) + ". Está no peso ideal");
+        }else if(textResultado>25.0 && textResultado<29.9){
+            resultadoTextView.setText("Seu IMC é " + df.format(textResultado) + ". Está levemente acima do peso ideal");
+        }else if(textResultado>30.0 && textResultado<34.9){
+            resultadoTextView.setText("Seu IMC é " + df.format(textResultado) + ". Está com obesidade grau 1");
+        }else if(textResultado>35.0 && textResultado<39.9){
+            resultadoTextView.setText("Seu IMC é " + df.format(textResultado) + ". Está com obesidade grau 2 (severa)");
+        } else{
+            resultadoTextView.setText("Seu IMC é " + df.format(textResultado) + ". Está com obesidade grau 3 (mórbida)");
+        }
+
     }
 }
